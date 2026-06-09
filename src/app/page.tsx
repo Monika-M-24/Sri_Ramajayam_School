3154c294507c97b225dd389ddb8c997776dc1b3a
Monika M <mmonikam2004@gmail.com>
2026-06-09 16:10:06 +0530

    Fix ParticlesBackground import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import ParticlesBackground from "@/components/ParticlesBackground";
+import ParticlesBackground from "../components/ParticlesBackground";
 
 import {
   FaSchool,
